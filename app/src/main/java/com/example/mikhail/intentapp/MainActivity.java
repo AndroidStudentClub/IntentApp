2167d4efbea66366657ceb211498aef35ad06c59
MikhailSkiy <Mikhail.Skiy@gmail.com>
2015-05-16 15:21:54 +0400

    Create actions to handle browser,camera,map and others intents

--- app/src/main/java/com/example/mikhail/intentapp/MainActivity.java
+++ app/src/main/java/com/example/mikhail/intentapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.mikhail.intentapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +33,65 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_map) {
+            openPrefferedLocationMap();
+            return true;
+        }
+        else if(id == R.id.action_gallery){
+            openGallery();
+            return  true;
+        }
+
+        else if(id == R.id.action_browser){
+            openBrowser();
+            return true;
+        }
+
+        else if(id == R.id.action_camera){
+            openCamera();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openGallery(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call because no receiving apps installed!");
+        }
+    }
+
+    private void openBrowser(){
+        Uri url = Uri.parse("http://www.google.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, url);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call because no receiving apps installed!");
+        }
+    }
+
+    private void openCamera(){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent,1);
+    }
+
+    private void openPrefferedLocationMap(){
+        Uri geoLocation = Uri.parse("geo:37.422006,-122.084095?").buildUpon().build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call because no receiving apps installed!");
+        }
     }
 }
